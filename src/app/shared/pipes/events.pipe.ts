de6931751fc715c38c8e87d6f24cc917fd8ac85f
Arthur Klein <kleinarthu@cy-tech.fr>
2023-11-07 17:07:52 +0100

    Search is no longer case-sensitive

--- src/app/shared/pipes/events.pipe.ts
+++ src/app/shared/pipes/events.pipe.ts
@@ -8,6 +8,7 @@ import { EvenementI } from '../models/evenement-i';
 export class EventsPipe implements PipeTransform {
 
   transform(listeEvents: Array<EvenementI>, filtre: string): Array<EvenementI> {
+	  filtre = filtre.toLowerCase();
 	  return listeEvents.filter(
 		  event => (
 			  event.titre.toLowerCase().indexOf(filtre) > -1 ||
